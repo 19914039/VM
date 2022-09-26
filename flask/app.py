2d346745228df267bf204956f0d2d10ba09461cc
Suresh <103111380+19914039@users.noreply.github.com>
2022-09-26 11:58:07 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -130,4 +130,4 @@ def api_delete(id):
             books.remove(book)
     return "Success: Book information has been deleted."
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
